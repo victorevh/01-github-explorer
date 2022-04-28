06083e8d44491bb7ec63fc5a6bc6ce67c481f03b
Victor Oliveira Santos <victor_oliveirasantos@hotmail.com>
2022-04-27 21:46:37 -0300

    Importa novo componente

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { RepositoryList } from './components/RepositoryList';
+import { ProfileItem } from './components/ProfileItem';
 import './styles/global.scss';
 
 export function App(){
     return (
         <>
+            <ProfileItem />
             <RepositoryList />
         </>
     )
---
+++ src/components/ProfileItem.tsx
@@ -0,0 +1,8 @@
+export function ProfileItem () {
+    return (
+        <div>
+            <h3>Victor Oliveira</h3>
+            <img src="./assets/profile.webp" alt="" />
+        </div>
+    );
+}
