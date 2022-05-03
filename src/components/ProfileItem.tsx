0daa1a81fe6a366aa32a09d0cf4c9aaf53dd956e
Victor Oliveira Santos <victor_oliveirasantos@hotmail.com>
2022-05-02 22:39:10 -0300

    Adiciona div About em componente profile

--- src/components/ProfileItem.tsx
+++ src/components/ProfileItem.tsx
@@ -3,8 +3,19 @@ import '../styles/profile.scss'
 export function ProfileItem () {
     return (
         <div className="profile">
-            <img src="" alt="" />
-            <h3>Victor Oliveira</h3>
+            <div className="divProfile">
+                <img
+                    className="imgProfile"
+                    src=""
+                    alt=""
+                    
+                />
+                <h3>Victor Oliveira</h3>
+            </div>
+            <div className="About">  
+                <h3>About me</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi recusandae eos inventore ducimus iste nam officiis earum cupiditate iure, expedita eaque deleniti, voluptatem cumque quibusdam nemo maxime, id incidunt quo? Lorem ipsum dolor sit amet consectetur adipisicing elit. A, dicta? Nesciunt, voluptas sapiente perferendis eos, quos quisquam esse delectus minus voluptates, sint omnis quae. Aspernatur fuga totam at asperiores necessitatibus.</p>
+            </div>
         </div>
     );
 }
