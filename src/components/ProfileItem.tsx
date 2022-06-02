0c95f661577a8962ee5dce1d45d35f53e2c8014a
Victor Oliveira Santos <victor_oliveirasantos@hotmail.com>
2022-06-02 20:20:13 -0300

    Adiciona texto profille

--- src/components/ProfileItem.tsx
+++ src/components/ProfileItem.tsx
@@ -14,7 +14,7 @@ export function ProfileItem () {
             </div>
             <div className="About">  
                 <h3>About me</h3>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi recusandae eos inventore ducimus iste nam officiis earum cupiditate iure, expedita eaque deleniti, voluptatem cumque quibusdam nemo maxime, id incidunt quo? Lorem ipsum dolor sit amet consectetur adipisicing elit. A, dicta? Nesciunt, voluptas sapiente perferendis eos, quos quisquam esse delectus minus voluptates, sint omnis quae. Aspernatur fuga totam at asperiores necessitatibus.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi recusandae eos inventore ducimus iste nam officiis earum cupiditate iure, expedita eaque deleniti, voluptatem cumque quibusdam nemo maxime, id incidunt quo? Lorem ipsum dolor sit amet consectetur adipisicing elit. A, dicta? Nesciunt, voluptas sapiente perferendis eos, quos quisquam esse delectus minus voluptates, sint omnis quae. Aspernatur fuga totam at asperiores necessitatibus. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore eligendi sapiente voluptate beatae fugit id nisi impedit ex accusamus ab aperiam aliquid, numquam dolores est consectetur, tempore perspiciatis cupiditate consequatur!</p>
             </div>
         </div>
     );
