02240bd45da4f17a7e73ff955ad80e112259b6f0
Victor Oliveira Santos <victor_oliveirasantos@hotmail.com>
2022-10-08 19:37:11 -0300

    Add import in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Header } from './components/Header'
-import { RepositoryList } from './components/RepositoryList';
 import { Profile } from './components/Profile';
+import { RepositoryList } from './components/RepositoryList';
+
 import './styles/global.scss';
 
 export function App(){
