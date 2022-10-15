ef955d8dc6cd9751e73be401a33628910d73f3d3
Victor Oliveira Santos <victor_oliveirasantos@hotmail.com>
2022-10-15 12:56:12 -0300

    Add NewContactModal function for App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
-import { Header } from './components/Header'
+import Modal from 'react-modal';
+import { useState } from 'react';
+import { Header } from './components/Header';
+import { NewContactModal } from './components/NewContactModal';
 import { Profile } from './components/Profile';
 import { RepositoryList } from './components/Repository/RepositoryList';
 import { GlobalStyle } from './styles/global';
 
+Modal.setAppElement('#root');
+
 export function App(){
+const [isNewContactModalOpen, setIsNewContactModalOpen] = useState(false);
+
+    function handleOpenNewContactModal() {
+        setIsNewContactModalOpen(true);
+    }
+
+    function handleCloseNewContactModal() {
+        setIsNewContactModalOpen(false);
+    }
+
     return (
         <>
-            <Header />
+            <Header onOpenNewContactModal={handleOpenNewContactModal} />
+            <NewContactModal
+                isOpen={isNewContactModalOpen}
+                onRequestClose={handleCloseNewContactModal}
+            />
             <Profile />
             <RepositoryList />
             <GlobalStyle />
